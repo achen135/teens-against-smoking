da3f9c21bfd06d24032a1fb16e71bd4cffe4a969
Alex <alexchen6607@gmail.com>
2024-06-25 14:31:48 -0400

    Created basic format for the hero

---
+++ src/Hero Component/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+<div className='hero-container'>
+    <div className='hero-bg'>
+        <div className='hero-title'>
+        Teens Against Smoking
+        </div>
+        <div className='hero-slogan'>
+        Join the movement, help teens quit smoking
+        </div>
+        <div className='hero-desc'>
+        Teens Against Smoking is a community of teens supporting each other to quit smoking and live healthier lives. Together, we can make a difference
+        </div>
+    </div>
+</div> 
+  )
+}
+
+export default Hero
