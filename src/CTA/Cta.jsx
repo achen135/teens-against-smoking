0ab2d88ad194fd16f7edc51f0e1fdd26b1f105dc
Alex <alexchen6607@gmail.com>
2024-06-28 20:48:16 -0400

    some better css for cta section

--- src/CTA/Cta.jsx
+++ src/CTA/Cta.jsx
@@ -14,15 +14,15 @@ const Cta = () => {
 
                  <ul className='resources-content'>
                     <li>
-                        <p>Getting help: If you or someone you know are affected by an underage smoking issue, please consider calling and receiving direct and professional support below.</p>
+                        <p><b>Getting help: </b>If you or someone you know are affected by an underage smoking issue, please consider calling and receiving direct and professional support below.</p>
                         <a href="https://www.samhsa.gov/find-help/national-helpline" target='_blank'>SAMHSA National helpline</a>
                     </li>
                     <li>
-                        <p>Becoming informed: Want to become more acquainted with information regarding teenage smoking? Check out this brief fact sheet from the American Lung Association below.</p>
+                        <p><b>Becoming informed:</b> Want to become more acquainted with information regarding teenage smoking? Check out this brief fact sheet from the American Lung Association below.</p>
                         <a href="https://www.lung.org/quit-smoking/smoking-facts/tobacco-use-among-children#:~:text=People%20who%20start%20smoking%20at,not%20able%20to%20do%20so." target='_blank'>American Lung Association</a>
                     </li>
                     <li>
-                        <p>Bringing Awareness/Helping others: Want to help bring awareness to or provide assistance to others affected by underage smoking but don’t know where to start? Don’t worry and check out these articles to help you get started!</p>
+                        <p><b>Bringing Awareness/Helping others:</b> Want to help bring awareness to or provide assistance to others affected by underage smoking but don’t know where to start? Don’t worry and check out these articles to help you get started!</p>
                         <a href="https://wristbandcreation.com/10-things-can-raise-awareness-cause/" target='_blank'>Wrist band creation </a>
                         <a href="https://www.lung.org/quit-smoking/helping-teens-quit" target='_blank'>American Lung Association</a>
                     </li>
