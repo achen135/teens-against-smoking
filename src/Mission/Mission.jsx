f441039e25f6ff519d331748e6224d5b2e59b8b2
rsern <joninshonobi@gmail.com>
2024-06-28 20:52:19 -0400

    removed unnecessary sentences from mission

--- src/Mission/Mission.jsx
+++ src/Mission/Mission.jsx
@@ -8,10 +8,8 @@ const Mission = () => {
         <h1 className='mission-title'>
             Empowering teens to quit smoking through peer support and education
         </h1>
-        <p className='mission-desc'>   
-            Empowering teens to quit smoking through peer support and education. 
-            At Teens Against Smoking, our mission is to provide a safe and supportive community for teens who are trying to quit smoking. 
-            What sets us apart is that our website is driven by teens helping teens. 
+        <p className='mission-desc'>     
+            At Teens Against Smoking, our mission is to provide a safe and supportive community for teens who are trying to quit smoking.  
             We believe that young people have a unique perspective and can better understand the challenges faced by their peers. 
             Through our platform, we offer resources, success stories, and a place for teens to connect and share their experiences. 
             Join us in the fight against smoking and together we can make a difference.
